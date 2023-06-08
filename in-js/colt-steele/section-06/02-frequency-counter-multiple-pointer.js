198fa3b366cc608aa820df8338104f0bc9cad74b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-08 22:11:40 +0530

    frequency counter multiple pointers

---
+++ in-js/colt-steele/section-06/02-frequency-counter-multiple-pointer.js
@@ -0,0 +1,24 @@
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+/*
+   
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true 
+areThereDuplicates('a', 'b', 'c', 'a') // true 
+*/
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a, b) => a > b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
+}
+
+areThereDuplicates(1, 2, 3);
