274469399ead06df93d7f9c9efb9c8d7d882ab8d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-08 22:13:03 +0530

    frequency counter one liners

---
+++ in-js/colt-steele/section-06/02-frequency-counter-one-liner.js
@@ -0,0 +1,14 @@
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+/*
+   
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true 
+areThereDuplicates('a', 'b', 'c', 'a') // true 
+*/
+
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
+
+areThereDuplicates(1, 2, 3);
