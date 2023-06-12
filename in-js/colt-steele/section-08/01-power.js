0174eaa3f7b21d4a8ac5d8ea22da8aa77c532b44
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-12 07:37:48 +0530

    power - bug fixed

--- in-js/colt-steele/section-08/01-power.js
+++ in-js/colt-steele/section-08/01-power.js
@@ -1,9 +1,8 @@
 function power(num1, num2){
-    if(num2 === 1){
-        return num1
+    if(num2 === 0){
+        return 1
     }
     return num1*power(num1, --num2)
-
 }
 
 console.log(power(2,3));
