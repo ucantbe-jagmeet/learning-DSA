be29d97b02bb08ff81d1268079ca83076b569816
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-17 08:32:56 +0530

    Binary Search

---
+++ in-js/colt-steele/section-10/02-binary-search.js
@@ -0,0 +1,20 @@
+function binarySearch( arr, num){
+
+    let start = 0
+    let end = arr.length -1 
+    let mid = Math.floor(( start + end )/2)
+
+    while( start <= end ){
+        if( num === arr[mid])  return mid
+        else if ( num < arr[mid]) {
+            end = mid -1
+        }
+        else {
+            start = mid + 1
+        }
+        mid = Math.floor(( start + end )/2)
+    }
+    return -1
+}
+
+console.log(binarySearch([ 2, 5, 6, 9, 13, 15, 28, 30], 13 ));
