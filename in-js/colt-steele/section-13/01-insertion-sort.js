7902be64ba2df55733be1fa554b631edb75dc14a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-20 06:07:41 +0530

    selection sort algorithm

---
+++ in-js/colt-steele/section-13/01-insertion-sort.js
@@ -0,0 +1,21 @@
+function insertionSort(arr){
+
+    for( var i = 1; i < arr.length ; i++){
+
+        var curr = arr[i]
+
+        for( var j = i - 1; j >= 0 && arr[j] > curr ; j--){
+            arr[j+1] = arr[j]
+        }
+        
+        arr[j+1] = curr
+    }
+
+    return arr
+}
+
+console.log(insertionSort([ 3, 44, 38, 5, 47, 15]));
+
+//  i , j , elem
+//  0 , 1 , 1
+//  0 , 2 , 2
