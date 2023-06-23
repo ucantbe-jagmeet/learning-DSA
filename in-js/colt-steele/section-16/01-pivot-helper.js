3dfaa857bd9869217fdbc2398b54b02cce660ffe
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-23 08:19:35 +0530

    pivot helper

---
+++ in-js/colt-steele/section-16/01-pivot-helper.js
@@ -0,0 +1,25 @@
+// pivot ([ 4, 8, 2, 1, 5, 7, 6,3])
+
+function swap( arr, i, j){
+    let temp = arr[i]
+    arr[i] == arr[j]
+    arr[j] = temp
+}
+
+function pivot( arr, start =0 , end = arr.length +1){
+    let pivot = arr[start]
+    var swapIndex = start
+
+   for( let i = start + 1 ; i < arr.length; i++){
+
+        if( pivot > arr[i]){
+            swapIndex ++
+            swap( arr, swapIndex, i)
+        }
+    }
+    swap( arr, start, swapIndex)
+
+    return swapIndex
+}
+
+console.log( pivot([ 4, 8, 2, 1, 5, 7, 6, 3]));
