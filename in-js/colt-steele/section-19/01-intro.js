8da2baff127745764469f3e6102d91ad4e7dd63a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-12 16:01:03 +0530

    push to singlyLinkedList

---
+++ in-js/colt-steele/section-19/01-intro.js
@@ -0,0 +1,11 @@
+/*
+    Lists 
+    -> Do not have indexes
+    -> Connected ia nodes with a next pointer
+    -> Random access is not allowed
+
+    Array
+    -> Indexed in order !
+    -> Insertion and deletion can be expensive
+    -> Can quickly be accessed at a specific index
+*/
