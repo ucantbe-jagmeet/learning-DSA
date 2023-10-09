372e00baec9993096bc125bb67ffc7c15cfe1e05
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-10-09 20:20:10 +0530

    check greatest

--- in-solidity/practice/02-factorial.sol
+++ in-solidity/practice/02-factorial.sol
@@ -9,10 +9,7 @@ contract FactorialCalculator {
         for (uint256 i = 2; i <= n; i++) {
             result *= i;
         }
-<<<<<<< HEAD
-=======
         
->>>>>>> 3130c5d14b0d675719a8fc11f7f5ece65c5467f6
         return result;
     }
 }
