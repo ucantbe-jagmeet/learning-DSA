9fb739e7309dc85eed71f3f526ac00ec5b97fdf0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-12-25 20:20:10 +0530

    qno 14

---
+++ in-js/leetcode/qno-14.js
@@ -0,0 +1,14 @@
+function temp(strs) {
+    if (!strs.length) return '';
+
+    for (let i = 0; i <= strs[0].length; i++) {
+        if (!strs.every((string) => string[i] === strs[0][i])) {
+            return strs[0].slice(0, i);
+        }
+    }
+
+    return strs[0];
+
+}
+
+temp(["flower", "flow", "flight"])
