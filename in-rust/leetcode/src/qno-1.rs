738d75ba2e822512bb66758563948bec06e8007e
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-15 15:55:40 +0530

    two sum in rust

---
+++ in-rust/leetcode/src/qno-1.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut hm = HashMap::new();
+    for (i, &num) in nums.iter().enumerate() {
+        if let Some(&j) = hm.get(&num) {
+            // If the complement exists, return the indices
+            return vec![j as i32, i as i32];
+        }
+        // Insert the complement of the current element (target - num) with its index
+        hm.insert(target - num, i);
+    }
+    vec![] // Return an empty vector if no solution is found
+}
+
+fn main() {
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    let indexes = two_sum(nums, target);
+    println!("Indexes are: {:?}", indexes);
+}
