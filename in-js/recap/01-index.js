06261d0d65d48ca0dcbbdd7bcd5e67ca8d46cec8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-06-23 15:12:09 +0530

    sum of n nnumber

---
+++ in-js/recap/01-index.js
@@ -0,0 +1,16 @@
+// sum of natural number from 1 to n
+
+function sum(num) {
+
+    // Step 1 
+    // let sum = 0 
+    // for (let i = 1; i <= num ;i++){
+    //     sum += i
+    // } 
+    // return sum
+
+    // step 2
+    return (num * (num + 1)) / 2
+}
+
+console.log(sum(6))
